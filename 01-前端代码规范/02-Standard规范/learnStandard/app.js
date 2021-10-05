d897977589ec469e952d92c23b0330950d7b6c01
lm101845 <1018457683@qq.com>
2021-10-05 14:52:21 +0800

    Auto commit.

---
+++ 01-前端代码规范/02-Standard规范/learnStandard/app.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: liming
+ * @Date: 2021-10-05 14:46:37
+ * @LastEditTime: 2021-10-05 14:47:42
+ * @FilePath: \learnStandard\app.js
+ */
+
+const a = 1 // 结尾不应该有分号，而且a定义了没有使用
+const f = () => { } // f定义了但是没有使用
+log() // log未定义
